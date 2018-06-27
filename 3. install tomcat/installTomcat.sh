075016d140be0aefcee2fb32441d035445bb7a2d
Deok mu <freefly3557@gmail.com>
2018-06-28 01:09:02 +0900

    feat : add information for changing server deploy path

--- 3. install tomcat/installTomcat.sh
+++ 3. install tomcat/installTomcat.sh
@@ -3,3 +3,5 @@
 sudo yum -y install wget
 wget http://apache.tt.co.kr/tomcat/tomcat-7/v7.0.88/bin/apache-tomcat-7.0.88.tar.gz
 tar zxvf apache-tomcat-7.0.88.tar.gz /opt/tomcat
+# replace server.xml
+# add <Context docBase="coffeekong" path="/"/>
